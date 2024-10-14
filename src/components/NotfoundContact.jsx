57606bf9ea8b0ea745f1bb4574afef1c7b3bfb97
professionaldev527 <professionaldev527@gmail.com>
2024-10-14 22:13:45 +0530

    Update

---
+++ src/components/NotfoundContact.jsx
@@ -0,0 +1,10 @@
+const NotfoundContact = () => {
+  return (
+    <div className="flex h-[80vh] items-center justify-center gap-2">
+      <img src="/contact.png" />
+      <h3 className="text-2xl font-medium text-white">Contact Not Found</h3>
+    </div>
+  );
+};
+
+export default NotfoundContact;
